bb8aae0e6d160d056055cdb3a772bda66c7bcb35
Alban KERLOC'H <akerloc-@e1r10p5.42.fr>
2019-10-12 17:01:57 +0200

    v r&r avec inifinite str

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: akerloc- <akerloc-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 11:31:17 by akerloc-          #+#    #+#             */
-/*   Updated: 2019/10/12 16:52:25 by akerloc-         ###   ########.fr       */
+/*   Updated: 2019/10/12 17:01:06 by akerloc-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int ft_printf(const char *format, ...)
     char *s;
     char *var1;
 	size_t i;
+	size_t k;
 	size_t t;
 	size_t len;
 	size_t	j;
@@ -30,6 +31,7 @@ int ft_printf(const char *format, ...)
 			t++;
 		i++;
 	}
+
 	va_start(ap, format);
 	printf("\n -%zu- \n", t);
 	i = 0;
@@ -42,11 +44,13 @@ int ft_printf(const char *format, ...)
 	}
 	va_end(ap);
 	printf("\n --%zu-- \n", len);
+
     s = NULL;
     if (!(s = (char*)malloc(ft_strlen(format) + len + 1)))
         return (0);
 	j = 0;
 	i = 0;
+	va_start(ap, format);
 	while (j < ft_strlen(format) + ft_strlen(var1))
 	{
 		if (format[i] != '%')
@@ -57,15 +61,20 @@ int ft_printf(const char *format, ...)
 		}
 		else
 		{
-			while (j < i + ft_strlen(var1))
+			var1 = va_arg(ap, char*);
+			printf("\n -%s- %zu %zu %zu\n", var1, j, i, j - i);
+			k = 0;
+			while (k < ft_strlen(var1))
 			{
-				s[j] = var1[j - i];
+				s[j] = var1[k];
 				j++;
+				k++;
 			}
 			i++;
 		}
 	}
 	s[j] = '\0';
+	va_end(ap);
 	ft_putstr(s);
 	return (0);
 }
