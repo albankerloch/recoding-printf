313e6b4cafff4ece3f884a2987ae34267c513ed2
Alban KERLOC'H <akerloc-@e1r9p2.42.fr>
2019-10-20 18:40:54 +0200

    conversion correctement corrigee

--- ft_check.c
+++ ft_check.c
@@ -6,7 +6,7 @@
 /*   By: akerloc- <akerloc-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 11:49:47 by akerloc-          #+#    #+#             */
-/*   Updated: 2019/10/20 18:15:25 by akerloc-         ###   ########.fr       */
+/*   Updated: 2019/10/20 18:39:09 by akerloc-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ size_t			ft_checktaille_min(va_list ap, const char *format, size_t *i)
 	if (format[*i + 1] == '*')
 	{
 		t = va_arg(ap, int);
+		var1 = (size_t)t;
 		var1 = t < 0 ? (size_t)(-t) : (size_t)(t); 
 		if (t < 0)
 			i[4] = 1;
@@ -58,7 +59,6 @@ size_t			ft_checktaille_min(va_list ap, const char *format, size_t *i)
 size_t			ft_checktaille_max(va_list ap, const char *format, size_t *i)
 {
 	size_t taille_max;
-	size_t	var1;
 	size_t	j;
 	int		t;
 
@@ -69,11 +69,12 @@ size_t			ft_checktaille_max(va_list ap, const char *format, size_t *i)
 		if (format[*i + 1] == '*')
 		{
 			t = va_arg(ap, int);
-			var1 = t < 0 ? -1 : (size_t)(t); 
+			taille_max = (size_t)t;
+			taille_max = t < 0 ? (size_t)(-1) : (size_t)(t); 
 			if (t < 0)
 				i[4] = 1;
 			*i = *i + 1;
-			return (var1);
+			return (taille_max);
 		}
 		else if (ft_isdigit(format[*i + 1]))
 		{
@@ -82,6 +83,7 @@ size_t			ft_checktaille_max(va_list ap, const char *format, size_t *i)
 				*i = *i + 1;
 			return (ft_atoi(&format[j + 1]));
 		}
+		return (0);
 	}
 	return (taille_max);
 }
